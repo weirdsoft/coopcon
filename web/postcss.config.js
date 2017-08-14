13789fb59dfe4afd11fb70f6a99c613268727f6e
Agustin Carrasco <asermax@gmail.com>
2017-08-13 23:21:50 -0300

    webpack: fixed postcss config

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -1,7 +1,8 @@
 /* eslint-env node */
 module.exports = {
-  'postcss-import': {},
-  'postcss-cssnext': {
-    browsers: [ 'last 2 versions', 'IE > 10' ],
+  'plugins': {
+    'postcss-cssnext': {
+      browsers: [ 'last 2 versions', 'IE > 10' ],
+    },
   },
 }

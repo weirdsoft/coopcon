19da5ed2233efbab3b5c0496a44957ecc0648a1d
Agustin Carrasco <asermax@gmail.com>
2017-11-12 18:52:58 -0300

    route: go to the first producer's operatives if not providing a route

--- web/src/data/route/init-sagas.js
+++ web/src/data/route/init-sagas.js
@@ -1,8 +1,22 @@
-import { put } from 'redux-saga/effects'
-import { fetchProducers } from 'data/producer/actions'
+import { put, take, select } from 'redux-saga/effects'
+import {
+  fetchProducers, FETCH_PRODUCERS_SUCCESS, FETCH_PRODUCERS_FAILURE,
+} from 'data/producer/actions'
+import { getProducers } from 'data/producer/selectors'
+  import { goToOperatives } from './actions'
 
 function* routeInitSaga() {
   yield put(fetchProducers())
+  const result = yield take([ FETCH_PRODUCERS_SUCCESS, FETCH_PRODUCERS_FAILURE ])
+
+  if (result.type === FETCH_PRODUCERS_SUCCESS) {
+    const producers = yield select(getProducers)
+    const first = producers[0]
+
+    if (first !== null) {
+      yield put(goToOperatives(first._id))
+    }
+  }
 }
 
 export default routeInitSaga

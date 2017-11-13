99ff140a87e7d29b78bec4f81a5993b1e4b7727a
Agustin Carrasco <asermax@gmail.com>
2017-11-12 22:50:32 -0300

    product: dont't replace the products object, update it

--- web/src/data/product/reducers.js
+++ web/src/data/product/reducers.js
@@ -1,28 +1,24 @@
 import { combineReducers } from 'redux'
 import { FETCH_PRODUCER_PRODUCTS_SUCCESS } from './actions'
 
-const ids = (state = [], action) => {
-  switch(action.type) {
-    case FETCH_PRODUCER_PRODUCTS_SUCCESS:
-      return action.products.map((product) => product._id)
-    default:
-      return state
-  }
-}
-
 const byId = (state = {}, action) => {
   switch(action.type) {
     case FETCH_PRODUCER_PRODUCTS_SUCCESS:
-      return action.products.reduce((result, product) => {
-        result[product._id] = product
-        return result
-      }, {})
+      return {
+        ...state,
+        ...action.products.reduce((result, product) => {
+          result[product._id] = {
+            ...state[product._id],
+            ...product,
+          }
+          return result
+        }, {}),
+      }
     default:
       return state
   }
 }
 
 export default combineReducers({
-  ids,
   byId,
 })

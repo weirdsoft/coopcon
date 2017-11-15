809d6dbbac5a8d7fead1681b91e012721b8e08f8
Agustin Carrasco <asermax@gmail.com>
2017-11-15 00:30:47 -0300

    producer: sort from older to newer

--- web/src/data/producer/selectors.js
+++ web/src/data/producer/selectors.js
@@ -3,7 +3,7 @@ import { createSelector } from 'reselect'
 export const getProducers = (state) => state.producer.list
 export const getSortedProducers = createSelector(
   [ getProducers ],
-  (producers) => producers.slice(0).sort((a, b) => (b.creationDate - a.creationDate)),
+  (producers) => producers.slice(0).sort((a, b) => (a.creationDate - b.creationDate)),
 )
 export const isLoadingProducers = (state) => state.producer.isLoading
 export const isAddingNewProducer = (state) => state.producer.isAdding

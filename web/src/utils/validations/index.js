3629d5a7a8422c1ff0208485d919ea6f59279dff
Agustin Carrasco <asermax@gmail.com>
2017-12-04 00:36:40 -0300

    validations: added a date validator

--- web/src/utils/validations/index.js
+++ web/src/utils/validations/index.js
@@ -1,7 +1,9 @@
+import validateDate from './validateDate'
 import validateNumber from './validateNumber'
 import validateString from './validateString'
 
 export default {
+  date: validateDate,
   number: validateNumber,
   string: validateString,
 }

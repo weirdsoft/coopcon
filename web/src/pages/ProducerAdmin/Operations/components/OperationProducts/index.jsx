6c6a0b1a5073d8cbe1833142fb644356c0285142
Agustin Carrasco <asermax@gmail.com>
2017-12-25 22:46:02 -0300

    operation products: added link to go back to the full operations table

--- web/src/pages/ProducerAdmin/Operations/components/OperationProducts/index.jsx
+++ web/src/pages/ProducerAdmin/Operations/components/OperationProducts/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { compose, branch, renderNothing, setDisplayName } from 'recompose'
+import { goToOperations } from 'data/route/actions'
+import { getCurrentId } from 'data/producer/selectors'
 import { isShowingOperationProducts } from 'data/operation/selectors'
+import Link from 'redux-first-router-link'
 
 const mapStateToProps = (state) => ({
+  producerId: getCurrentId(state),
   isShowingProducts: isShowingOperationProducts(state),
 })
 
@@ -16,9 +20,18 @@ const enhancer = compose(
   setDisplayName('OperationProducts'),
 )
 
-const OperationProducts = enhancer(() => (
+const OperationProducts = enhancer(({ producerId }) => (
   <div className="col-8">
-    Products
+    <div className="card">
+      <div className="card-body">
+        <Link to={goToOperations(producerId)} className="close">
+          <span aria-hidden="true">
+            ×
+          </span>
+        </Link>
+        Products
+      </div>
+    </div>
   </div>
 ))
 

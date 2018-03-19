2fa67c22feee1d73e514e7224fc8b9c42bcb0bd3
Agustin Carrasco <asermax@gmail.com>
2018-03-18 23:45:25 -0300

    operation: added floating action to add a new order

--- mobile/activities/Operation/index.js
+++ mobile/activities/Operation/index.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react'
 import * as R from 'ramda'
 import { connect } from 'react-redux'
 import { compose, setDisplayName } from 'recompose'
+import { goToOrder } from 'Coopcon/data/navigation/actions'
 import { getOrderIds } from 'Coopcon/data/order/selectors'
 import { StyleSheet, View, FlatList } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+import { FloatingAction } from 'react-native-floating-action'
 import Order from './components/Order'
 
 const styles = StyleSheet.create({
@@ -16,17 +19,30 @@ const mapStateToProps = (state) => ({
   orders: R.map((id) => ({ id }), getOrderIds(state)),
 })
 
+const mapDispatchToProps = (dispatch) => ({
+  goToOrder: () => dispatch(goToOrder()),
+})
+
 const enhancer = compose(
-  connect(mapStateToProps),
+  connect(mapStateToProps, mapDispatchToProps),
   setDisplayName('Home'),
 )
 
-const Operation = enhancer(({ orders }) => (
+const Operation = enhancer(({ orders, goToOrder }) => (
   <View style={styles.container}>
     <FlatList
       data={orders}
       keyExtractor={R.prop('id')}
       renderItem={({ item: { id } }) => (<Order id={id} />)}
+    />
+    <FloatingAction
+      actions={[ {
+        position: 1,
+        name: 'add',
+        icon: (<MaterialIcons name="add" size={25} color="white"/>),
+      } ]}
+      overrideWithAction={true}
+      onPressItem={goToOrder}
     />
   </View>
 ))

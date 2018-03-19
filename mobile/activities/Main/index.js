5f7daaa060dff9656ed8f03c4f4e047a81d745f2
Agustin Carrasco <asermax@gmail.com>
2018-03-19 00:12:28 -0300

    mobile: handle hardware back press on android

--- mobile/activities/Main/index.js
+++ mobile/activities/Main/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { compose, setDisplayName } from 'recompose'
-import { addNavigationHelpers } from 'react-navigation'
+import { compose, withHandlers, lifecycle, setDisplayName } from 'recompose'
+import { BackHandler } from 'react-native'
+import { addNavigationHelpers, NavigationActions } from 'react-navigation'
 import { createReduxBoundAddListener } from 'react-navigation-redux-helpers'
 import { getMainStack } from 'Coopcon/data/navigation/selectors'
 import { MainStack } from 'Coopcon/data/navigation/navigators'
@@ -11,8 +12,30 @@ const mapStateToProps = (state) => ({
   addListener: createReduxBoundAddListener('mainStack'),
 })
 
+const mapDispatchToProps = (dispatch) => ({
+  goBack: () => dispatch(NavigationActions.back()),
+  dispatch,
+})
+
 const enhancer = compose(
-  connect(mapStateToProps),
+  connect(mapStateToProps, mapDispatchToProps),
+  withHandlers({
+    onBackPress: ({ mainStack, goBack }) => () => {
+      if (mainStack.index !== 0) {
+        goBack()
+      }
+
+      return mainStack.index !== 0
+    },
+  }),
+  lifecycle({
+    componentDidMount() {
+      BackHandler.addEventListener("hardwareBackPress", this.props.onBackPress)
+    },
+    componentWillUnmount() {
+      BackHandler.removeEventListener("hardwareBackPress", this.props.onBackPress)
+    },
+  }),
   setDisplayName('Main'),
 )
 

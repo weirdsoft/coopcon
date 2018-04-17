dfd8aaa823231586e3b1258f5d26fc0379962b10
Agustin Carrasco <asermax@gmail.com>
2018-04-16 21:47:39 -0300

    operations: use FlatList's refresh controller instead of my custom hoc

--- mobile/activities/Home/index.js
+++ mobile/activities/Home/index.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 import * as R from 'ramda'
 import { connect } from 'react-redux'
 import { compose, setDisplayName } from 'recompose'
+import { fetchOperations } from 'Coopcon/data/operation/actions'
 import { isLoadingOperations, getOperationIds } from 'Coopcon/data/operation/selectors'
 import { StyleSheet, View, FlatList } from 'react-native'
-import withLoadingIndicator from 'Coopcon/hocs/withLoadingIndicator'
 import Operation from './components/Operation'
 
 const styles = StyleSheet.create({
@@ -18,15 +18,20 @@ const mapStateToProps = (state) => ({
   operations: R.map((id) => ({ id }), getOperationIds(state)),
 })
 
+const mapDispatchToProps = (dispatch) => ({
+  fetchOperations: () => dispatch(fetchOperations()),
+})
+
 const enhancer = compose(
-  connect(mapStateToProps),
-  withLoadingIndicator(),
+  connect(mapStateToProps, mapDispatchToProps),
   setDisplayName('Home'),
 )
 
-const Home = enhancer(({ operations }) => (
+const Home = enhancer(({ loading, operations, fetchOperations }) => (
   <View style={styles.container}>
     <FlatList
+      onRefresh={fetchOperations}
+      refreshing={loading}
       data={operations}
       keyExtractor={R.prop('id')}
       renderItem={({ item: { id } }) => (<Operation id={id} />)}

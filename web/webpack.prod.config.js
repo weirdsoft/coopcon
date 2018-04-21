aa7862a809d023d212d78527b5a556ccdb7ca4bc
Agustin Carrasco <asermax@gmail.com>
2018-04-21 12:38:28 -0300

    web: added webpack clean plugin

--- web/webpack.prod.config.js
+++ web/webpack.prod.config.js
@@ -1,10 +1,12 @@
 /* eslint-env node */
 const webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 const config = require('./webpack.base.config')
 
 config.mode = 'production'
 config.output.filename = '[name].[chunkhash].js'
 config.plugins[1].options.includeGTM = true
 config.plugins.push(new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /es/))
+config.plugins.push(new CleanWebpackPlugin([ 'dist/' ]))
 
 module.exports = config

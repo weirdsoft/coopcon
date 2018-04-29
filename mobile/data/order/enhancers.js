f61a9524ca5cc743965c3ba1d7c6e43d3ec371f6
Agustin Carrasco <asermax@gmail.com>
2018-04-29 14:40:31 -0300

    order: add analytics when viewing (toggling) an order

--- mobile/data/order/enhancers.js
+++ mobile/data/order/enhancers.js
@@ -1,9 +1,10 @@
 import * as R from 'ramda'
 import { TRACK_WITH_PROPS } from 'Coopcon/data/segment/types'
 import { getProduct } from 'Coopcon/data/product/selectors'
+import { getCurrentOrderId } from './selectors'
 import {
   ADD_PRODUCT_TO_ORDER, ADD_TO_PRODUCT_QUANTITY, SUBTRACT_TO_PRODUCT_QUANTITY,
-  TOGGLE_PAID_ORDER_SUCCESS,
+  TOGGLE_ORDER, TOGGLE_PAID_ORDER_SUCCESS,
 } from './actions'
 
 const addMinimalFractionAsQuantity = (getState, action) => R.merge({
@@ -25,9 +26,28 @@ const addAnalyticsOnPaidOrder = (getState, action) => R.when(
   }),
 )(action)
 
+const addAnalyticsOnToggleOrder = (getState, action) => R.when(
+  R.compose(
+    R.not,
+    R.propEq('id', getCurrentOrderId(getState())),
+  ),
+  R.mergeDeepLeft({
+    meta: {
+      analytics: {
+        eventType: TRACK_WITH_PROPS,
+        eventName: 'Order viewed',
+        eventData: {
+          orderId: action.id,
+        },
+      },
+    },
+  }),
+)(action)
+
 export default {
   [ADD_PRODUCT_TO_ORDER]: addMinimalFractionAsQuantity,
   [ADD_TO_PRODUCT_QUANTITY]: addMinimalFractionAsQuantity,
   [SUBTRACT_TO_PRODUCT_QUANTITY]: addMinimalFractionAsQuantity,
+  [TOGGLE_ORDER]: addAnalyticsOnToggleOrder,
   [TOGGLE_PAID_ORDER_SUCCESS]: addAnalyticsOnPaidOrder,
 }

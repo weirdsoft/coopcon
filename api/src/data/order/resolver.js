8825f5b662e7868b8dee9dd3d0e6bbc5b0a3a384
Agustin Carrasco <asermax@gmail.com>
2018-06-01 02:24:22 -0300

    order: fixed remove product, small fix on add product

--- api/src/data/order/resolver.js
+++ api/src/data/order/resolver.js
@@ -20,13 +20,13 @@ const resolver = {
 
       const productId = mongoose.Types.ObjectId(orderProduct.product)
       const order = await Order.findById(id).exec()
-      let product = R.find(R.propEq('product', productId), order.products)
+      let product = R.find(R.propEq('product', productId))(order.products)
 
       if (R.isNil(product)) {
         order.products.push({ product: productId, quantity: orderProduct.quantity })
         product = R.last(order.products)
       } else {
-        product.quantity = orderProduct.quantity
+        product.quantity += orderProduct.quantity
       }
 
       await order.save()
@@ -37,14 +37,21 @@ const resolver = {
       return product
     },
     async removeOrderProduct(_, { id, orderProduct }) {
-      const order = await Order.findById(id).exec()
-      let product = R.find(R.propEq('product', orderProduct.product), order.products)
+      const count = await Product.find({ _id: orderProduct.product }).count().exec()
 
-      if (product.quantity < orderProduct.quantity) {
+      if (count === 0) {
+        throw 'The product does not exist'
+      }
+
+      const productId = mongoose.Types.ObjectId(orderProduct.product)
+      const order = await Order.findById(id).exec()
+      let product = R.find(R.propEq('product', productId))(order.products)
+
+      if (product.quantity > orderProduct.quantity) {
         product.quantity -= orderProduct.quantity
       } else {
-        product.remove()
-        product = null
+        product.quantity = 0
+        order.products = R.reject(R.equals(product))(order.products)
       }
 
       await order.save()

be7c8e4077a4132a31726607e904d04fdd2c3cd7
Agustin Carrasco <asermax@gmail.com>
2018-06-16 16:33:45 -0300

    user: added user model

---
+++ api/src/data/user/model.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose'
+
+const UserSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  photo: { type: String, required: true },
+})
+
+const User = mongoose.model('User', UserSchema)
+
+export default User

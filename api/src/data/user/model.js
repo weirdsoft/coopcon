0686b9ea6c40db46f70f7173ec836b0322564546
Agustin Carrasco <asermax@gmail.com>
2018-06-17 18:00:07 -0300

    user: added isAdmin property to the model

--- api/src/data/user/model.js
+++ api/src/data/user/model.js
@@ -4,6 +4,7 @@ const UserSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   photo: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
 })
 
 const User = mongoose.model('User', UserSchema)

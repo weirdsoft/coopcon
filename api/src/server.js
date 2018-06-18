cccaeb50395c40fab98ec201e282a48cbfe79f3c
Agustin Carrasco <asermax@gmail.com>
2018-06-17 22:27:16 -0300

    api: added user to the GraphQL context

--- api/src/server.js
+++ api/src/server.js
@@ -1,11 +1,13 @@
 import '@babel/polyfill'
+import * as R from 'ramda'
 import mongoose from 'mongoose'
 import express from 'express'
 import bodyParser from 'body-parser'
-import { graphqlExpress, graphiqlExpress } from 'graphql-server-express'
-import { MONGO_URL, PORT } from './config'
-import { schema } from './data'
-import { configureAuth } from './auth'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+import { MONGO_URL, PORT } from 'config'
+import { schema } from 'data'
+import { ROLES } from 'data/user'
+import { configureAuth } from 'auth'
 
 // mongoose
 mongoose.Promise = Promise
@@ -25,6 +27,11 @@ const port = PORT
 configureAuth(app)
 app.use(bodyParser.json())
 app.use('/api/graphiql', graphiqlExpress({ endpointURL: '/api' }))
-app.use('/api', graphqlExpress({ schema }))
+app.use('/api', graphqlExpress((req) => ({
+  schema,
+  context: {
+    user: R.propOr({ role: ROLES.GUEST }, 'user')(req),
+  },
+})))
 
 app.listen(port)

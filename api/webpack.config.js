aeea7c5798815a6f3e96eeb40a65394cd84546d9
Agustin Carrasco <asermax@gmail.com>
2018-06-22 00:41:53 -0300

    api: use webpack nodemon plugin, remove unused dependencies

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,25 +1,34 @@
 const { resolve }  = require('path')
 const nodeExternals = require('webpack-node-externals')
+const NodemonPlugin = require( 'nodemon-webpack-plugin' ) // Ding
 
-module.exports = function() {
-  return {
-    entry: 'server',
-    target: 'node',
-    externals: [ nodeExternals() ],
-    output: {
-      filename: 'bundle.js',
-      path: resolve(__dirname, 'dist'),
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          use: [
-            'babel-loader',
-          ],
-          exclude: /node_modules/,
-        },
-      ],
-    },
-  }
+module.exports = {
+  entry: resolve(__dirname, 'src', 'server'),
+  target: 'node',
+  externals: [ nodeExternals() ],
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    modules: [
+      'src',
+      'node_modules',
+    ],
+    extensions: [ '.js' ],
+  },
+  plugins: [
+    new NodemonPlugin(),
+  ],
 }
